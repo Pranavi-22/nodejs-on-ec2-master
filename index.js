495dea821898b4670886acd4cfd508ecd5a21f25
Pranavi_ap <60108462+Pranavi-22@users.noreply.github.com>
2023-10-15 03:09:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. THis is TECHESSAY, pranavi hello'); //write a response to the client
+  res.write('You are  in AWS  Cloud. THis is TECHESSAY, pranavi hi hello'); //write a response to the client
   res.end(); //end the response
 }).listen(5000); //the server object listens on port 4000
